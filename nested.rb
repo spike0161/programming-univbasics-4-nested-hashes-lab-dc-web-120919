3ba9506c83834b5bc3177cc5b767e1c1cf60349a
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-08-30 16:26:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-   return programmer_hash = [:dennis_ritchie][:languages]
+   return programmer_hash[:dennis_ritchie][:languages]
     
 end
 
