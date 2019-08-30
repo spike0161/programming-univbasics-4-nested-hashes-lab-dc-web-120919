c6b61fdf798efe75a4265195657a5e8c6a5e02e1
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-08-30 16:33:31 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,17 +58,20 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+   return programmer_hash[:dennis_ritchie][:languages].first
+    
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+
 
 	programmer_hash = 
  		{
+ 		   :yukihiro_matsumoto => {
+         :known_for => "Ruby",
+         :languages => ["LISP", "C"]
+ },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -79,7 +85,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+return programmer_hash
     
 end
 
@@ -101,7 +107,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     return programmer_hash
      
 end
 
@@ -121,5 +128,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+     return programmer_hash
 
 end
