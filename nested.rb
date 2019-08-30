63e74c34e2d77f54039fd138ec197f41fb9d592a
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-08-30 16:28:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,10 +64,7 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+#
 
 	programmer_hash = 
  		{
